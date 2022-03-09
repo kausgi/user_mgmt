675958b3d24281f5775a130135ca8cc37be6166a
kausgi <86883014+kausgi@users.noreply.github.com>
2022-03-10 02:05:42 +0530

    Update Address.java

--- src/main/java/com/example/user_mgmt/Address.java
+++ src/main/java/com/example/user_mgmt/Address.java
@@ -1,1 +1,27 @@
-package com.example.user_mgmt;import lombok.*;import javax.persistence.Column;import javax.persistence.Embeddable;@Embeddable@Getter@Setter@NoArgsConstructor@AllArgsConstructor@ToStringpublic class Address {    @Column(name = "Flat_No")    private int flat_no;    @Column(name = "City")    private String city;    @Column(name = "County")    private String county;    @Column(name = "District")    private String district;    @Column(name = "Country")    private String country;}
+//
+
+package com.example.user_mgmt;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Address {
+    @Column(name = "Flat_No")
+    private int flat_no;
+    @Column(name = "City")
+    private String city;
+    @Column(name = "County")
+    private String county;
+    @Column(name = "District")
+    private String district;
+    @Column(name = "Country")
+    private String country;
+}
