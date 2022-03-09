fc0aaac03bcb9fdac38148c2cd698323f8429675
kausgi <86883014+kausgi@users.noreply.github.com>
2022-03-10 02:04:52 +0530

    Update UserRepository.java

--- src/main/java/com/example/user_mgmt/UserRepository.java
+++ src/main/java/com/example/user_mgmt/UserRepository.java
@@ -1,1 +1,10 @@
-package com.example.user_mgmt;import org.springframework.data.jpa.repository.JpaRepository;public interface UserRepository extends JpaRepository<User, Integer> {}
+//
+
+package com.example.user_mgmt;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
+
