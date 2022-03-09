2ffe14969072b3f0b77094d5d203c3374e96d971
kausgi <86883014+kausgi@users.noreply.github.com>
2022-03-10 02:06:06 +0530

    Update Laptop.java

--- src/main/java/com/example/user_mgmt/Laptop.java
+++ src/main/java/com/example/user_mgmt/Laptop.java
@@ -1,1 +1,23 @@
-package com.example.user_mgmt;import lombok.*;import javax.persistence.Column;import javax.persistence.Embeddable;@Embeddable@Getter@Setter@AllArgsConstructor@NoArgsConstructor@ToStringpublic class Laptop {    @Column(name = "Model_No")    private int lid;    @Column(name = "Model_Name")    private String lname;    @Column(name = "Model_price")    private double price;}
+//
+
+package com.example.user_mgmt;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Laptop {
+    @Column(name = "Model_No")
+    private int lid;
+    @Column(name = "Model_Name")
+    private String lname;
+    @Column(name = "Model_price")
+    private double price;
+}
