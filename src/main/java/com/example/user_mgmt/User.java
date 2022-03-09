1f60e6f2e0d31b26eceb97f7e7bdc312e8de3d99
kausgi <86883014+kausgi@users.noreply.github.com>
2022-03-10 02:06:35 +0530

    Update User.java

--- src/main/java/com/example/user_mgmt/User.java
+++ src/main/java/com/example/user_mgmt/User.java
@@ -1,1 +1,29 @@
-package com.example.user_mgmt;import lombok.*;import javax.persistence.*;@Entity@Getter@Setter@NoArgsConstructor@AllArgsConstructor@ToString@Table(name = "user_details")public class User {    @Id    @GeneratedValue(strategy = GenerationType.AUTO)    private int id;    private String username;    private String name;    private int age;    @Embedded    private Address address;    private String phone;    @Embedded    private Laptop laptop;    private double salary;}
+//
+
+package com.example.user_mgmt;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name = "user_details")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String username;
+    private String name;
+    private int age;
+    @Embedded
+    private Address address;
+    private String phone;
+    @Embedded
+    private Laptop laptop;
+    private double salary;
+}
